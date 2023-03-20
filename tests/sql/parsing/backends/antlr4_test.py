a9c986adbbc09ec746581e854eb0d92a7fd8ca57
samredai <sredai@netflix.com>
2023-03-19 23:42:42 -0400

    a few tests for antlr4 backend

---
+++ tests/sql/parsing/backends/antlr4_test.py
@@ -0,0 +1,99 @@
+"""
+Tests for custom antlr4 parser
+"""
+
+import pytest
+
+from dj.sql.parsing.backends.antlr4 import parse
+
+
+@pytest.mark.parametrize(
+    "query_string",
+    [
+        """SELECT suit, key, value
+    FROM suites_and_ranks_arrays
+    LATERAL VIEW EXPLODE(rankmap) AS key, value
+    ORDER BY suit;""",
+        """SELECT suit, exploded_rank
+    FROM suites_and_ranks_arrays
+    LATERAL VIEW EXPLODE(rank) exploded_rank
+    ORDER BY suit;""",
+        """SELECT suit, exploded_rank, key, value
+    FROM suites_and_ranks_arrays
+    LATERAL VIEW EXPLODE(rank) AS exploded_rank
+    LATERAL VIEW EXPLODE(rankmap) AS key, value
+    ORDER BY suit;""",
+    ],
+)
+def test_antlr4_backend_lateral_view_explode(query_string):
+    """
+    Test LATERAL VIEW EXPLODE queries
+    """
+    parse(query_string)
+
+
+@pytest.mark.parametrize(
+    "query_string",
+    [
+        """Select suit, exploded_rank, exploded_rank2
+    from suites_and_ranks_arrays
+    CROSS JOIN UNNEST(rank) as t(exploded_rank)
+    ORDER BY suit;""",
+        """Select suit, exploded_rank, exploded_rank2
+    from suites_and_ranks_arrays
+    CROSS JOIN UNNEST(rank, rank) as t(exploded_rank, exploded_rank2)
+    ORDER BY suit;""",
+        """Select suit, exploded_rank, exploded_rank2
+    from suites_and_ranks_arrays
+    CROSS JOIN UNNEST(rank) as t(exploded_rank)
+    CROSS JOIN UNNEST(rank) as t(exploded_rank2)""",
+        """Select suit, key, value
+    from suites_and_ranks_arrays
+    CROSS JOIN UNNEST(rankmap) as t(key, value)
+    ORDER BY suit;""",
+        """Select suit, exploded_rank, k, value
+    from suites_and_ranks_arrays
+    CROSS JOIN UNNEST(rank, rankmap) as t(exploded_rank, key, value)
+    ORDER BY suit;""",
+    ],
+)
+def test_antlr4_backend_cross_join_unnest(query_string):
+    """
+    Test CROSS JOIN UNNEST queries
+    """
+    parse(query_string)
+
+def test_antlr4_backend_predicate_like():
+    """
+    Test LIKE predicate
+    """
+    query = parse("SELECT * FROM person WHERE name LIKE '%$_%';")
+    assert "LIKE '%$_%'" in str(query)
+
+def test_antlr4_backend_predicate_ilike():
+    """
+    Test ILIKE predicate
+    """
+    query = parse("SELECT * FROM person WHERE name ILIKE '%foo%';")
+    assert "ILIKE '%foo%'" in str(query)
+
+def test_antlr4_backend_predicate_rlike():
+    """
+    Test RLIKE predicate
+    """
+    query = parse("SELECT * FROM person WHERE name RLIKE 'M+';")
+    assert "RLIKE 'M+'" in str(query)
+
+def test_antlr4_backend_predicate_is_distinct_from():
+    """
+    Test IS DISTINCT FROM predicate
+    """
+    query = parse("SELECT * FROM person WHERE name IS DISTINCT FROM 'Bob'")
+    assert "IS DISTINCT FROM 'Bob'" in str(query)
+
+def test_antlr4_backend_trim():
+    """
+    Test trim
+    """
+    query = parse("SELECT trim(BOTH FROM '    SparkSQL   ');")
+    assert "TRIM('    SparkSQL   ')" in str(query)
