9d3b58a493abe9f239980984e82e2553594fbe99
samredai <sredai@netflix.com>
2023-03-27 20:39:28 -0400

    compile tests

--- tests/sql/parsing/test_ast2.py
+++ tests/sql/parsing/test_ast2.py
@@ -8,12 +8,23 @@ from dj.errors import DJException
 from dj.sql.parsing.backends.antlr4 import parse
 from dj.sql.parsing.ast2 import CompileContext
 
-@pytest.mark.parametrize("query", [
+
+def test_ast_compile_table(session, client_with_examples):
+    query_ast = parse("SELECT hard_hat_id, last_name, first_name FROM hard_hats")
+    exc = DJException()
+    ctx = CompileContext(session=session, query=query_ast, exception=exc)
+    query_ast.select.from_.relations[0].primary.compile(ctx)
+    assert not exc.errors
+
+
+@pytest.mark.parametrize("query, table", [
     ("SELECT a from foo", "foo"),
 ])
-def test_ast_compile_query(session, query, table):
+def test_ast_compile_table_missing_node(session, query, table):
     query_ast = parse(query)
     exc = DJException()
     ctx = CompileContext(session=session, query=query_ast, exception=exc)
     query_ast.select.from_.relations[0].primary.compile(ctx)
     assert f"No node `{table}` exists of kind" in exc.errors[0].message
+
+
