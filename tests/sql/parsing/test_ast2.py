50d503478c496e52b79e8aeee63e60307920e772
samredai <sredai@netflix.com>
2023-03-27 20:41:11 -0400

    Test docstring

--- tests/sql/parsing/test_ast2.py
+++ tests/sql/parsing/test_ast2.py
@@ -10,6 +10,11 @@ from dj.sql.parsing.ast2 import CompileContext
 
 
 def test_ast_compile_table(session, client_with_examples):
+    """
+    Test compiling the primary table from a query
+    
+    Includes client_with_examples fixture so that examples are loaded into session
+    """
     query_ast = parse("SELECT hard_hat_id, last_name, first_name FROM hard_hats")
     exc = DJException()
     ctx = CompileContext(session=session, query=query_ast, exception=exc)
