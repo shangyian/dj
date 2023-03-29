40df9846910633f66167fa6c2c8380a0385bc302
samredai <sredai@netflix.com>
2023-03-28 20:43:07 -0400

    more compile tests

--- tests/sql/parsing/test_ast2.py
+++ tests/sql/parsing/test_ast2.py
@@ -22,8 +22,10 @@ def test_ast_compile_table(session, client_with_examples):
     assert not exc.errors
 
 
-
 def test_ast_compile_table_missing_node(session):
+    """
+    Test compiling a table when the node is missing
+    """
     query_ast = parse("SELECT a FROM foo")
     exc = DJException()
     ctx = CompileContext(session=session, query=query_ast, exception=exc)
@@ -55,3 +57,26 @@ def test_ast_compile_table_missing_node(session):
     assert f"No node `foo` exists of kind" in exc.errors[0].message
     query_ast.select.from_.relations[0].extensions[0].right.select.from_.relations[0].primary.compile(ctx)
     assert f"No node `bar` exists of kind" in exc.errors[1].message
+
+
+def test_ast_compile_query(session, client_with_examples):
+    """
+    Test compiling an entire query
+    """
+    query_ast = parse("SELECT hard_hat_id, last_name, first_name FROM hard_hats")
+    exc = DJException()
+    ctx = CompileContext(session=session, query=query_ast, exception=exc)
+    query_ast.compile(ctx)
+    assert not exc.errors
+
+
+def test_ast_compile_query_missing_columns(session, client_with_examples):
+    """
+    Test compiling a query with missing columns
+    """
+    query_ast = parse("SELECT hard_hat_id, column_foo, column_bar FROM hard_hats")
+    exc = DJException()
+    ctx = CompileContext(session=session, query=query_ast, exception=exc)
+    query_ast.compile(ctx)
+    assert "Column`column_foo` does not exist on any valid table." in exc.errors[0].message
+    assert "Column`column_bar` does not exist on any valid table." in exc.errors[1].message
